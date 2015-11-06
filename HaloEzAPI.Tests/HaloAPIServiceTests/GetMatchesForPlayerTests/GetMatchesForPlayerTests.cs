573fe204568c4ee74c82dab398df92301a48e877
glitch100 <glitch100@gmail.com>
2015-11-06 14:06:19 +0000

    tests and license

---
+++ HaloEzAPI.Tests/HaloAPIServiceTests/GetMatchesForPlayerTests/GetMatchesForPlayerTests.cs
@@ -0,0 +1,55 @@
+﻿using HaloEzAPI.Abstraction.Enum;
+using HaloEzAPI.Model.Response.Error;
+using NUnit.Framework;
+
+namespace HaloEzAPI.Tests.HaloAPIServiceTests.GetMatchesForPlayerTests
+{
+    [TestFixture]
+    public class GetMatchesForPlayerTests
+    {
+        private HaloAPIService _haloApiService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _haloApiService = new HaloAPIService("YOURTOKENHERE");
+        }
+
+        [TestCase("Glitch100", GameMode.Arena)]
+        public void Default_DoesNotThrowException(string gamerTag,GameMode gameMode)
+        {
+            Assert.DoesNotThrow(async () => await _haloApiService.GetMatchesForPlayer(gamerTag, gameMode));
+        }
+
+        [TestCase("Glitch100", GameMode.Arena)]
+        public async void Default_DoesNotReturnNull(string gamerTag,GameMode gameMode)
+        {
+            var result = await _haloApiService.GetMatchesForPlayer(gamerTag, gameMode);
+            Assert.IsNotNull(result);
+        }
+
+        [TestCase("",GameMode.Arena)]
+        [TestCase(null,GameMode.Arena)]
+        [TestCase("asfasd9f8adf89andfa98sdfa",GameMode.Arena)]
+        public void ProvideInvalidGamertag_ThrowsException(string gamerTag,GameMode gameMode)
+        {
+            Assert.Throws<HaloAPIException>(async () => await _haloApiService.GetMatchesForPlayer(gamerTag, gameMode),CommonErrorMessages.InvalidGamerTag);
+        }
+
+        [TestCase("Glitch100")]
+        public async void ProvideValidGamertag_GetValidReponse(string gamerTag)
+        {
+            var result = await _haloApiService.GetMatchesForPlayer(gamerTag,GameMode.All);
+            Assert.AreEqual(result.Count,25);
+        }
+
+        [TestCase("Glitch100",5,10)]
+        [TestCase("Glitch100",6,15)]
+        public async void ProvideStartAndCount_GetValidReponse(string gamerTag,int start, int count)
+        {
+            var result = await _haloApiService.GetMatchesForPlayer(gamerTag, GameMode.All, start, count);
+            Assert.AreEqual(result.Count, count);
+            Assert.AreEqual(result.Start, start);
+        }
+    }
+}
---
+++ HaloEzAPI.Tests/HaloAPIServiceTests/HaloAPIServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HaloEzAPI.Tests.HaloAPIServiceTests
+{
+    [TestFixture]
+    public class HaloAPIServiceTests
+    {
+        private HaloAPIService _haloApiService;
+
+        [Test]
+        public void Default_DoesntThrowException()
+        {
+            Assert.DoesNotThrow(() =>  _haloApiService = new HaloAPIService("TOKEN"));   
+        }
+    }
+}
