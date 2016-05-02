0e00917ae808050d2899fccdcf27e413712af8ad
Jon Evans <glitch100@gmail.com>
2016-05-01 21:21:47 -0500

    1.2.4

--- HaloEzAPI/Model/Response/Stats/ArenaPostGameReport.cs
+++ HaloEzAPI/Model/Response/Stats/ArenaPostGameReport.cs
@@ -6,7 +6,7 @@ namespace HaloEzAPI.Model.Response.Stats
     public class ArenaPostGameReport : MatchDetails
     {
         public IEnumerable<ArenaPlayerStat> PlayerStats { get; set; }
-        public Guid SeasonId { get; set; }
+        public Guid ?SeasonId { get; set; }
         public IEnumerable<TeamStat> TeamStats { get; set; } 
     }
 }
